823908212e9c5650ca61989b8a0dcccc95d42e5d
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-06-04 20:55:33 -0600

    finish pattrn party"

--- js/index.js
+++ js/index.js
@@ -10,12 +10,24 @@ $.fn.invisible = function () {
 };
 
 $accordionButton.on('click', function() {
-  $accordionPanel.each(function(index, panel){
-    $(panel).invisible();
-  })
-  $(this).next('.accordion-panel').visible();
-  $accordionButton.removeClass('active');
-  $(this).addClass('active');
+  if (window.innerWidth <= 800) {
+    if ($(this).next('.accordion-panel').is(':visible')) {
+      $(this).toggleClass('active-mobile');
+      $(this).next('.accordion-panel').css({ display: 'none'});
+    } else {
+      $accordionButton.removeClass('active-mobile');
+      $(this).addClass('active-mobile');
+      $('.accordion-panel').css({ display: 'none' });
+      $(this).next('.accordion-panel').css({ display: 'flex' });
+    }
+  } else {
+    $accordionPanel.each(function(index, panel){
+      $(panel).invisible();
+    })
+    $(this).next('.accordion-panel').visible();
+    $accordionButton.removeClass('active');
+    $(this).addClass('active');
+  }
 });
 
 $('.code-button').on('click', function(event) {
@@ -31,8 +43,18 @@ $('.menu-btn-grp').on('click', function(event) {
 });
 
 $(window).resize(function() {
-  if ((window).width() > 699) {
+  if ($(window).width() > 699) {
     $('.header-bar a').show();
     $('.header-bar input').show();
+  }
+
+  if (window.innerWidth <= 800) {
+    $('.accordion-panel').css({
+      display: 'none',
+      visibility: 'visible'
+    });
+  } else {
+    $('.accordion-panel').css({ display: 'flex' });
+    $accordionButton.removeClass('active-mobile');
   }
 })
